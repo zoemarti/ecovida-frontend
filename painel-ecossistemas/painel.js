a5f73c2d2f3b757a4bb24352249a454685b6415b
thcerutti <thiagocerutti@gmail.com>
2025-07-09 20:51:51 -0300

    atualiza estrutura de dados dos biomas e ajusta mensagem de descrição no painel

--- painel-ecossistemas/painel.js
+++ painel-ecossistemas/painel.js
@@ -1,13 +1,13 @@
 function carregarConteudo(bioma) {
     const conteudo = document.getElementById('conteudo');
-    fetch('./biomas.json')
-      .then(response => response.json())
-      .then(data => {
-        console.log('Arquivo biomas.json carregado com sucesso:', data);
+    fetch("./biomas.json")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Dados carregados:", data);
+        conteudo.innerHTML = `<h2>${data.biomas[bioma].nome}</h2>
+                          <div class="descricao">${data.biomas[bioma].descricao}</div>`;
       })
-      .catch(error => {
-        console.error('Erro ao carregar o arquivo biomas.json:', error);
+      .catch((error) => {
+        console.error("Erro ao carregar o arquivo biomas.json:", error);
       });
-    conteudo.innerHTML = `<h2>${bioma}</h2>
-                          <div class="descricao">Descrição do bioma ${bioma} com saiba mais</div>`;
 }
