19ae0af9b0f4bb148d5d886e95150d995c452411
thcerutti <thiagocerutti@gmail.com>
2025-07-16 18:05:24 -0300

    feat: adiciona imagens dos biomas no painel de ecossistemas

--- painel-ecossistemas/painel.js
+++ painel-ecossistemas/painel.js
@@ -5,7 +5,8 @@ function carregarConteudo(bioma) {
       .then((data) => {
         console.log("Dados carregados:", data);
         conteudo.innerHTML = `<h2>${data.biomas[bioma].nome}</h2>
-                          <div class="descricao">${data.biomas[bioma].descricao}</div>`;
+                          <div class="descricao">${data.biomas[bioma].descricao}</div>
+                          <img src="${data.biomas[bioma].imagem}" alt="${data.biomas[bioma].nome}" class="imagem-bioma">`;
       })
       .catch((error) => {
         console.error("Erro ao carregar o arquivo biomas.json:", error);
